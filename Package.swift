707b29c45c2f37a28ed3fa9efc216327136a30c4
Justinas Krištopaitis <justinas.kristopaitis@obdeleven.com>
2024-05-29 13:31:25 +0300

    Package

--- Package.swift
+++ Package.swift
@@ -1,21 +1,24 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Boomboarding",
+    platforms: [
+        .iOS(.v13),  // Specify the minimum iOS version
+        .macOS(.v10_15)  // Specify the minimum macOS version
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Boomboarding",
             targets: ["Boomboarding"]),
     ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Boomboarding"),
+            name: "Boomboarding",
+            dependencies: []),
         .testTarget(
             name: "BoomboardingTests",
             dependencies: ["Boomboarding"]),
